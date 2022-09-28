7cad43477e9f144d1da0a6defe4899d857ae26de
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2022-09-28 17:23:09 -0500

    another try

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,14 +5,11 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: "pizza",
-      types: [
-        { id: "1", type: " Cheese" },
-        { id: "2", type: "Vegetarian" },
-        { id: "3", type: "Meat Lover" },
-        { id: "4", type: "All In" },
-        { id: "5", type: "Build My Own" }
-
+      value: "coconut",
+      countries: [
+        { id: "1", country: "Cambodia" },
+        { id: "2", country: "Australia" },
+        { id: "3", country: "US" }
       ]
     };
 
@@ -20,7 +17,7 @@ class App extends React.Component {
   }
 
   handleSubmit(event) {
-    alert("Your selected type is: " + this.state.value);
+    alert("Your favorite flavor is: " + this.state.value);
     event.preventDefault();
   }
 
@@ -28,6 +25,21 @@ class App extends React.Component {
     this.setState({ value: event.target.value });
   };
 
+  getUnique(arr, comp) {
+    const unique = arr
+      //store the comparison values in array
+      .map(e => e[comp])
+
+      // store the keys of the unique objects
+      .map((e, i, final) => final.indexOf(e) === i && i)
+
+      // eliminate the dead keys & store unique objects
+      .filter(e => arr[e])
+
+      .map(e => arr[e]);
+
+    return unique;
+  }
 
   render() {
     const countries = require("./countries.json");
@@ -45,7 +57,22 @@ class App extends React.Component {
         </label>
         <br />
         <br />
-        <form>
+        <label>
+          Looping through Array
+          <select>
+            {this.state.countries.map(item => (
+              <option key={item.id} value={item.country}>
+                {item.country}
+              </option>
+            ))}
+            {console.log(this.state.countries)}
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+        <br />
+        <br />
+        <label>
+          Looping through Json File
           <select>
             {uniqueCountry.map(item => (
               <option key={item.id} value={item.country}>
